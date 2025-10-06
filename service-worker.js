2c8e241d6f92c815fecfa46da75e0dacaa10dbc2
Vinicius Fidelis <160151701+fidelissss@users.noreply.github.com>
2025-10-06 12:14:50 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,31 @@
-const CACHE_NAME = 'controle-turmas-v1';
+const CACHE_NAME = 'controle-turmas-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/turmas.html',
-  '/turma-form.html',
-  '/alunos.html',
-  '/aluno-form.html',
-  '/disciplinas.html',
-  '/disciplina-form.html',
-  '/login.html',
-  '/minha-area.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.webmanifest',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  
+  // Ícones
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  
+  // Screenshots (opcionais)
+  './screenshots/mobile-screenshot.png',
+  './screenshots/desktop-screenshot.png'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('Cache aberto');
         return cache.addAll(urlsToCache);
       })
   );
@@ -30,11 +35,8 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+        // Retorna do cache ou faz fetch
+        return response || fetch(event.request);
+      })
   );
 });
